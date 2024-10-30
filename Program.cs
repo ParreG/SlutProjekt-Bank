9521c493c13a3531eba02928f87d85e88eb632ff
ParmanGitijah <Parman.gitijah@chasacademy.se>
2024-10-30 09:51:39 +0100

    Klar med början av menyn

--- Program.cs
+++ Program.cs
@@ -1,10 +1,14 @@
-﻿namespace SlutProjekt_Bank
+﻿using SlutProjekt_Bank.Classes;
+
+namespace SlutProjekt_Bank
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            
+            Bank bank = new Bank();
+
+            bank.ShowMenu();
         }
     }
 }
