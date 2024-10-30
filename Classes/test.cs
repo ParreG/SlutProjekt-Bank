2ead40c7e4aabce0115da83f5fada93bea94f7e8
Korkken <Korkken@gmail.com>
2024-10-30 09:47:52 +0100

    test

---
+++ Classes/test.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SlutProjekt_Bank.Classes
+{
+    internal class test
+    {
+    }
+}
