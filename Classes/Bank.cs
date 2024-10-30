89f18d9a548060215010af01e5a138dc33e4d4a8
ParmanGitijah <Parman.gitijah@chasacademy.se>
2024-10-30 21:39:05 +0100

    fixa i menyn. import av Encoding Unicode. 4 menyval. Pilen är nu jämt.

--- Classes/Bank.cs
+++ Classes/Bank.cs
@@ -12,26 +12,29 @@ namespace SlutProjekt_Bank.Classes
 
         public void ShowMenu()
         {
-            //En flagga som håller koll på om programmet är aktivt. Denna hjälper mig att avsluta programmet ganska direkt om programavslut knappen trycks i menyn. 
+            
             bool programAktivt = true;
 
-            //Variabel för att hålla reda på valt menyval.
+           
             int menuSelected = 0;
 
-            //Lägger till menyval i en array.Samtigit lägger jag en tappsteg på de olika valen bara för desgin mässig. 
-            string[] menuOptions = new string[] { "Menyval1\t", "Menyval2\t\t", "Menyval3\t\t", "Menyval3\t\t" };
+             
+            string[] menuOptions = new string[] { "Menyval1\t\t", "Menyval2\t\t", "Menyval3\t\t", "Menyval4\t\t", "Avsluta programmet\t" };
 
             while (programAktivt)
             {
                 Console.Clear();
                 Console.CursorVisible = false;
 
-                //Loopar igenom menyvalen och markerar det valda alternativet. Detta gör att jag inte behöver repetera samma del flera gånger. 
+                Console.WriteLine("Hej och välkommen till bankens meny.");
+                Console.WriteLine("Du kan navigera med \" ⬇️\" och \" ⬆️\". \nTryck på \"Enter\" när du vill välja den menyn du är nöjd med.");
+                Console.WriteLine();
+
                 for (int i = 0; i < menuOptions.Length; i++)
                 {
                     if (i == menuSelected)
                     {
-                        //För att menyn ska synas bättre la jag till en pil innan menyn också
+                        
                         Console.WriteLine("->" + menuOptions[i] + " <------");
                     }
                     else
@@ -40,10 +43,10 @@ namespace SlutProjekt_Bank.Classes
                     }
                 }
 
-                //Läser in användarens tangenttryck.
+                
                 var keyPressed = Console.ReadKey();
 
-                //Börjar en If sats. Denna tillåter användaren att scrolla genom menyn med pilen ner eller upp knappen.
+                
                 if (keyPressed.Key == ConsoleKey.DownArrow && menuSelected != menuOptions.Length - 1)
                 {
                     menuSelected++;
@@ -53,15 +56,15 @@ namespace SlutProjekt_Bank.Classes
                     menuSelected--;
                 }
 
-                //Om användaren trycker på ENTER så väljs det alternativet i menyn. 
+                
                 else if (keyPressed.Key == ConsoleKey.Enter)
                 {
                     Console.Clear();
 
-                    //Beroende på användarens val i menyn, tar programmet upp olika funktioner.
+                    
                     switch (menuSelected)
                     {
-                        //Börjar på 0 då Arrayen börjar på 0.
+                        
                         case 0:
 
                             //MENYVAL1();
