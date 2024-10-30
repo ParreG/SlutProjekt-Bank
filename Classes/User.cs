e2c71a97b3d76a92d6ff35e2584dcd1f6048c301
Lifehack25 <126723076+Lifehack25@users.noreply.github.com>
2024-10-30 11:46:04 +0100

    Skapat base klassen User samt 2 barn, Admin & Client. Har även skapat en statisk klass "Security" som kommer innehålla saker kring inloggning och annat kring säkerhet.

---
+++ Classes/User.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SlutProjekt_Bank.Classes
+{
+    public abstract class User(string password, string name, string surName, string email, int phoneNumber, string address, int postalCode, string city, string country)
+    {
+        public Guid UserID = Guid.NewGuid();
+        public string Password = password;
+        public string Name = name;
+        public string Surname = surName;
+        public string Email = email;
+        public int Phone = phoneNumber;
+
+        // Prints the user information
+        public void UserInformation()
+        {
+            Console.WriteLine("User ID: " + UserID);
+            Console.WriteLine("Name: " + Name);
+            Console.WriteLine("Surname: " + Surname);
+            Console.WriteLine("Email: " + Email);
+            Console.WriteLine("Phone: " + Phone);
+        }
+    }
+}
